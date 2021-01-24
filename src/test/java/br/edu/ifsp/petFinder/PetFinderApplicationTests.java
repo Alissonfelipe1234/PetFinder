02758f19f10daf1c274af27c932934dcc8378c37
alisson.f.silva <alisson.f.silva@kroton.com.br>
2021-01-24 20:44:13 -0300

    primeiro commit

---
+++ src/test/java/br/edu/ifsp/petFinder/PetFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.petFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
